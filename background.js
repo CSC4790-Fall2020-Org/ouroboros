6f5875576f4ff24b88417288737c74037d3f76f1
Michael Norce <32281629+mnorce@users.noreply.github.com>
2020-09-11 11:22:46 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -1,2 +1,6 @@
-$("*").highlight(['data','location','information']);
+$("p:contains('data')").css("background-color","#FFFF00");
+$("p:contains('location')").css("background-color","#FFFF00");
+$("p:contains('information')").css("background-color","#FFFF00");
+$("p:contains('personal')").css("background-color","#FFFF00");
+
 alert("Look out for these key words in the TOS:\nData, Location, Personal information");
