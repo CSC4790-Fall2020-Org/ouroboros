4e64faa7c0c16a73a2152fa87f314b2e778fbd56
farrell444 <62407963+farrell444@users.noreply.github.com>
2020-09-25 13:35:26 -0400

    Add files via upload
    Working scaper that scapes the webpage and outputs the top n keywords in console. Only working on Twitter privacy right now. Still need to figure out how to pull the full xpath of webpage in function and not manually input it.

--- scraper.js
+++ scraper.js
@@ -1,20 +1,59 @@
 const puppeteer = require('puppeteer');
 
-async function scrapeKeywords(url) {
 
+  getTopNWords('https://twitter.com/en/privacy',10);
+  //getTopNWords('https://www.facebook.com/help/instagram/196883487377501',10);
+  //getTopNWords('https://www.snap.com/en-US/privacy/privacy-policy',10);
+
+  async function getTopNWords(url,n)
+  {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
-    const [el] = await page.$x('/html/body/main');
+    const [el] = await page.$x('/html/head');
     const text = await el.getProperty('textContent');
     const name = await text.jsonValue();
-    var n = name.search("data"); //Contains all words from site
-    //var split;
-    //var result = string1.localeCompare(string2);  //Need to work on parsing the string
-    browser.close();
-    console.log({n}); //
-    return {n}
+        //var n = name.search("data"); //Contains all words from site
+        //var split;
+        //var result = string1.localeCompare(string2);  //Need to work on parsing the string
+        //var text = name.search("data");
 
-}
-scrapeKeywords('https://twitter.com/en/privacy'); //Inputing webpage for now but will auto grab it 
+        // Find 'em!
+
+    browser.close();
+
+      var wordRegExp = /\w+(?:'\w{1,2})?/g;
+      var words = {};
+      var matches;
+      while ((matches = wordRegExp.exec(name)) != null)
+      {
+          var word = matches[0].toLowerCase();
+          if (typeof words[word] == "undefined")
+          {
+              words[word] = 1;
+          }
+          else
+          {
+              words[word]++;
+          }
+      }
+
+      var wordList = [];
+      for (var word in words)
+      {
+          if (words.hasOwnProperty(word))
+          {
+              wordList.push([word, words[word]]);
+          }
+      }
+      wordList.sort(function(a, b) { return b[1] - a[1]; });
+
+      var topWords = [];
+      for (var i = 0; i < n; i++)
+      {
+          topWords.push(wordList[i][0]);
+      }
+      console.log({topWords});
+      //return topWords;
+  }
