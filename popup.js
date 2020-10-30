b59d91537e505eea1753cdedb339947fc92f059e
farrell444 <62407963+farrell444@users.noreply.github.com>
2020-10-29 23:40:14 -0400

    Complete Rewrite of popup.js
    Rewrote the script now using chrome api to allow for globalization. Some improvements made to the scraping algorithm.

--- popup.js
+++ popup.js
@@ -1,20 +1,83 @@
-function doIt() {
-  alert("Ouroboros has scanned the page\nWe've highlighted instances of \"data,\" \"person-,\" and \"priv-\"");
-}
-function awesomeTask() {
-  doIt();
+function scrapeThePage() {
+  // Gets HTML of current tab
+  var htmlCode = document.documentElement.innerHTML;
+  //Scrper algorithm that parses all HTML code and content matches to our predetermined list of most important keywords
+  var wordRegExp = /\w+(?:'\w{1,2})?/g;
+    var words = {};
+    var matches;
+    while ((matches = wordRegExp.exec(htmlCode)) != null)
+    {
+        var word = matches[0].toLowerCase();
+        if (typeof words[word] == "undefined")
+        {
+            words[word] = 1;
+        }
+        else
+        {
+            words[word]++;
+        }
+    }
+
+    var wordList = [];
+    for (var word in words)
+    {
+        if(word !=="false" && word !=="quot"){
+          if (words.hasOwnProperty(word)) 
+          {
+              if (word = "data"){
+                  wordList.push([word, words[word]]);
+              }
+          }
+        }
+    }
+    wordList.sort(function(a, b) { return b[1] - a[1]; });
+
+    var topWords = [];
+    for (var i = 0; i < 5; i++)
+    {
+        topWords.push(wordList[i][0]);
+    }
+
+    for(var i=0;i<topWords.length;i++){
+      topWords[i]= (i+1)+ ". "+topWords[i];
+    }
+    var stringOfKeywords = topWords.toString();
+    function uppercase(str)
+    {
+      var array1 = str.split(' ');
+      var newarray1 = [];
+
+      for(var x = 0; x < array1.length; x++){
+          newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+      }
+      return newarray1.join(' ');
+    }
+    uppercase(stringOfKeywords);
+
+    var lower = stringOfKeywords.replace(/,/g, ", ");
+    final =lower.toUpperCase();
+    console.log({final});
+    return final;
 }
 
-function clickHandler(e) {
-  setTimeout(awesomeTask, 0);
-}
+document.addEventListener('DOMContentLoaded', () => {
+  // Hook up #check-1 button in popup.html
+  const fbshare = document.querySelector('#check-1');
+  fbshare.addEventListener('click', async () => {
+      // Get the active tab
+      const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+      const tab = tabs[0];
+      console.log(tab);
+      // We have to convert the function to a string
+      const scriptToExec = `(${scrapeThePage})()`;
 
-function main() {
-  // Initialization work goes here.
-}
+      // Run the script in the context of the tab
+      const scraped = await chrome.tabs.executeScript(tab.id, { code: scriptToExec });
 
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', clickHandler);
-  main();
+      // Result will be an array of values from the execution
+      // For testing this will be the same as the console output if you ran scriptToExec in the console
+      alert(scraped[0]);
+      //console.log(scraped[0]);
+      //alert(tab);
+  });
 });
