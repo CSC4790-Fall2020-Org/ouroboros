5bb070204d0b8e800ce31f10feddf8fdc6875352
farrell444 <62407963+farrell444@users.noreply.github.com>
2020-10-29 23:43:52 -0400

    Delete background.js
    No longer used.

--- background.js
+++
@@ -1,6 +0,0 @@
-$("p:contains('data')").css("background-color","#FFFF00");
-$("p:contains('location')").css("background-color","#FFFF00");
-$("p:contains('information')").css("background-color","#FFFF00");
-$("p:contains('personal')").css("background-color","#FFFF00");
-
-alert("Look out for these key words in the TOS:\nData, Location, Personal information");
